59f25c3d1b21657cf271fbb93229c0b1059993e1
lazarusBK <nelsopon13@gmail.com>
2024-03-14 15:24:12 -0300

    Estructura SQL modificada

--- structure.sql
+++ structure.sql
@@ -3,29 +3,29 @@ CREATE DATABASE db_vinyls;
 USE db_vinyls;
 
 CREATE TABLE `users` (
-   `id` INT NOT NULL AUTO_INCREMENT,
+   `user_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
    `name` VARCHAR(25) NOT NULL,
    `user` VARCHAR(25) NOT NULL,
    `email` VARCHAR(100) NOT NULL,
-   `password` VARCHAR(20) NOT NULL,
+   `password` VARCHAR(100) NOT NULL,
    `avatar_path` VARCHAR(255) NOT NULL,
    `rol` VARCHAR(10) NOT NULL,
    `created_at` DATETIME,
    `updated_at` DATETIME,
-   PRIMARY KEY (`id`)
+   PRIMARY KEY (`user_id`)
 );
 
 CREATE TABLE `products` (
-   `product_id` INT NOT NULL,
-   `artist_id` INT NOT NULL,
-   `genre_id` INT NOT NULL,
+   `product_id` INT UNSIGNED NOT NULL,
+   `artist_id` INT UNSIGNED NOT NULL,
+   `genre_id` INT UNSIGNED NOT NULL,
    `product_title` VARCHAR(50) NOT NULL,
    `product_info` MEDIUMTEXT NOT NULL,
    `price` DECIMAL(10,2) NOT NULL,
    `tracklist` MEDIUMTEXT NOT NULL,
    `stock` INT NOT NULL,
-   `release_date` DATE NOT NULL,
-   `label_id` INT NOT NULL,
+   `release_date` YEAR NOT NULL,
+   `label_id` INT UNSIGNED NOT NULL,
    `cover_path` VARCHAR(255) NOT NULL,
    `created_at` DATETIME,
    `updated_at` DATETIME,
@@ -33,7 +33,7 @@ CREATE TABLE `products` (
 );
 
 CREATE TABLE `genres` (
-   `genre_id` INT NOT NULL,
+   `genre_id` INT  UNSIGNED NOT NULL,
    `genre_name` VARCHAR(50) NOT NULL,
    `genre_info` MEDIUMTEXT NOT NULL,
    `genre_path` VARCHAR(255) NOT NULL,
@@ -43,16 +43,16 @@ CREATE TABLE `genres` (
 );
 
 CREATE TABLE `user_products` (
-   `carrito_id` INT AUTO_INCREMENT,
-   `user_id` INT NOT NULL,
-   `product_id` INT NOT NULL,
+   `carrito_id` INT  UNSIGNED AUTO_INCREMENT,
+   `user_id` INT UNSIGNED NOT NULL,
+   `product_id` INT UNSIGNED NOT NULL,
    `created_at` DATETIME,
    `updated_at` DATETIME,
    PRIMARY KEY (`carrito_id`)
 );
 
 CREATE TABLE `labels` (
-   `label_id` INT NOT NULL,
+   `label_id` INT UNSIGNED NOT NULL,
    `label_name` VARCHAR(50) NOT NULL,
    `label_info` MEDIUMTEXT NOT NULL,
    `label_path` VARCHAR(255),
@@ -61,8 +61,17 @@ CREATE TABLE `labels` (
    PRIMARY KEY (`label_id`)
 );
 
+CREATE TABLE `label_products` (
+	`labelProd_id` INT UNSIGNED AUTO_INCREMENT,
+    `label_id` INT UNSIGNED NOT NULL,
+    `product_id` INT UNSIGNED NOT NULL,
+    `created_at` DATETIME,
+   `updated_at` DATETIME,
+   PRIMARY KEY (`labelProd_id`)
+);
+
 CREATE TABLE `artists` (
-   `artist_id` INT NOT NULL,
+   `artist_id` INT UNSIGNED NOT NULL,
    `artist_name` VARCHAR(50) NOT NULL,
    `artist_info` MEDIUMTEXT NOT NULL,
    `artist_path` VARCHAR(255) NOT NULL,
@@ -78,6 +87,6 @@ ALTER TABLE `products` ADD CONSTRAINT `FK_e1f16e94-918c-49bc-a9a8-2e74a1bc8966` 
 
 ALTER TABLE `products` ADD CONSTRAINT `FK_4da40f4c-3a56-4d56-ab56-75f8d1383d0a` FOREIGN KEY (`genre_id`) REFERENCES `genres`(`genre_id`)  ;
 
-ALTER TABLE `user_products` ADD CONSTRAINT `FK_04b1cb78-314d-4d77-9ca2-02ecc8339d99` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)  ;
+ALTER TABLE `user_products` ADD CONSTRAINT `FK_04b1cb78-314d-4d77-9ca2-02ecc8339d99` FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`)  ;
 
 ALTER TABLE `user_products` ADD CONSTRAINT `FK_7eae830e-03cf-4473-98db-98f241187d1a` FOREIGN KEY (`product_id`) REFERENCES `products`(`product_id`)  ;
